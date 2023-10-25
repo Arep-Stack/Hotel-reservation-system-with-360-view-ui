1a310ac5617e27bfebbeb06a565ac8fe2d2cccf5
Justine Dave M. Delos reyes <delosreyes.justinedave@gmail.com>
2023-10-25 13:36:00 +0800

    HRWV3V-20: Setup project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+import { MantineProvider, createTheme } from '@mantine/core';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@mantine/core/styles.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const themeColors = [
+  '#e9fbef',
+  '#dcf0e3',
+  '#bcdec7',
+  '#99c9a9',
+  '#7bb990',
+  '#68af7f',
+  '#5daa76',
+  '#4c9465',
+  '#408557',
+  '#307349',
+];
+
+const theme = createTheme({
+  colors: {
+    themeColors,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
